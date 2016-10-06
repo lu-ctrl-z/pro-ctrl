a881a551461c0f8ab163474f8634213b964d5c73
tinh1188 <vantinhnov88@gmail.com>
2016-10-06 16:55:44 +0700

    modify

--- assets/js/__d.js
+++ assets/js/__d.js
@@ -3,6 +3,11 @@ __d = function(a, b, c) {
 };
 $(function() {
     __d('click', '#cmdFullScreen', function() {
+        if(!$('#appManager').hasClass('isFullScreen')) {
+            $('#appManager').addClass('isHideSideRight');
+        } else {
+            $('#appManager').removeClass('isHideSideRight');
+        }
         $('#appManager').toggleClass('isFullScreen')
     });
     __d('click', '#cmdHideSideRight', function() {
@@ -11,5 +16,4 @@ $(function() {
     __d('click', '#cmdMinHeader', function() {
         $('#appManager').toggleClass('isMinHeader')
     });
-    
 })
