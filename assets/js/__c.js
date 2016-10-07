c05046e134e318f05186fc48100a4922ca773ee0
tinh1188 <vantinhnov88@gmail.com>
2016-10-07 11:40:23 +0700

    #001 boottraping

--- assets/js/__c.js
+++ assets/js/__c.js
@@ -1,23 +1,3 @@
 $(function() {
-    var RESIZE = {startX: null, stopX: null, el: null};
-    var resizer = document.getElementById('Hsplit');
-    var LeftSidebarContainer = document.getElementById('LeftSidebarContainer');
-    var MainContainer = document.getElementById('MainContainer');
-    resizer.addEventListener('mousedown', initDrag, false);
-    function initDrag(e) {
-        RESIZE.el = e.target;
-        RESIZE.startX = e.clientX;
-        document.documentElement.addEventListener('mousemove', doDrag, false);
-        document.documentElement.addEventListener('mouseup', stopDrag, false);
-    }
-    function doDrag(e) {
-        RESIZE.el.style.left = (e.clientX) + 'px';
-        LeftSidebarContainer.style.width = (e.clientX) + 'px';
-        MainContainer.style.left = (e.clientX + 8) + 'px';
-    }
-    function stopDrag(e) {
-        document.documentElement.removeEventListener('mousemove', doDrag, false);
-        document.documentElement.removeEventListener('mouseup', stopDrag, false);
-    }
 })
 
--- assets/js/__d.js
+++ assets/js/__d.js
@@ -1,6 +1,7 @@
 __d = function(a, b, c) {
     $('body').on(a, b, c);
 };
+
 $(function() {
     __d('click', '#cmdFullScreen', function() {
         if(!$('#appManager').hasClass('isFullScreen')) {
@@ -16,4 +17,29 @@ $(function() {
     __d('click', '#cmdMinHeader', function() {
         $('#appManager').toggleClass('isMinHeader')
     });
+    /*Process Resize left container*/
+    var Resizer = {startX: null, stopX: null, el: null};
+    function initDrag(e) {
+        var appManager = $('#appManager');
+        if(appManager.hasClass('isFullScreen')) return;
+        Resizer.el = e.target;
+        Resizer.startX = e.clientX;
+        document.documentElement.addEventListener('mousemove', doDrag, false);
+        document.documentElement.addEventListener('mouseup', stopDrag, false);
+    }
+    var fn_resizer = false;
+    function doDrag(e) {
+        if(fn_resizer) clearTimeout(fn_resizer);
+        fn_resizer = setTimeout(function() {
+            $(Resizer.el).css("left", e.clientX);
+            $('#LeftSidebarContainer').css('width', e.clientX);
+            $('#MainContainer').css('left', e.clientX + 8);
+        }, 0);
+    }
+    function stopDrag(e) {
+        document.documentElement.removeEventListener('mousemove', doDrag, false);
+        document.documentElement.removeEventListener('mouseup', stopDrag, false);
+    }
+    __d('mousedown', '#Hsplit',  initDrag);
+    /*End Process Resize left container*/
 })
