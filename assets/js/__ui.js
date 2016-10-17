1819cf37bd0bfa48ad12d27f98b901c284fff46c
tinh1188 <tinhbv@d2tsolution.com>
2016-10-17 12:03:55 +0700

    #001 table

--- assets/js/__ui.js
+++ assets/js/__ui.js
@@ -63,8 +63,212 @@
           $scope.setActive($scope.tabs[0]);
   }]),
   function() {
+      "use strict";
+      angular.module("app.tables", ['ngStorage']).controller("tableCtrl", ["$scope", "$filter", "$localStorage", function($scope, $filter, $localStorage) {
+          var init;
+          $scope.config = {
+              pull: "/pull/",
+              push: "/push/",
+              commit: "",
+              showIndex: true,
+              canAdd: true,
+              canEdit: true,
+              canDelete: true,
+          };
+          return $scope.stores = [{
+              code: "100110001",
+              cycl: -0.25,
+              kind: "Mắt 1.56",
+              desciption: "Mắt cận loại thường",
+              number: 20,
+              price: 180000,
+            }, {
+              code: "100110001",
+              cycl: -0.25,
+              kind: "Mắt 1.56",
+              desciption: "Mắt cận loại thường",
+              number: 20,
+              price: 180000,
+          }, {
+              code: "100110001",
+              cycl: -0.25,
+              kind: "Mắt 1.56",
+              desciption: "Mắt cận loại thường",
+              number: 20,
+              price: 180000,
+          }, {
+              code: "100110001",
+              cycl: -0.25,
+              kind: "Mắt 1.56",
+              desciption: "Mắt cận loại thường",
+              number: 20,
+              price: 180000,
+          }, {
+              code: "100110054",
+              cycl: -0.25,
+              kind: "Mắt 1.56",
+              desciption: "Mắt cận loại thường",
+              number: 20,
+              price: 180000,
+          }, {
+              code: "100110001",
+              cycl: -0.25,
+              kind: "Mắt 1.56",
+              desciption: "Mắt cận loại thường",
+              number: 20,
+              price: 180000,
+          }, {
+              code: "100110001",
+              cycl: -0.25,
+              kind: "Mắt 1.56",
+              desciption: "Mắt cận loại thường",
+              number: 20,
+              price: 180000,
+          }, {
+              code: "100110001",
+              cycl: -0.25,
+              kind: "Mắt 1.56",
+              desciption: "Mắt cận loại thường",
+              number: 20,
+              price: 180000,
+          }, {
+              code: "100110001",
+              cycl: -0.25,
+              kind: "Mắt 1.56",
+              desciption: "Mắt cận loại thường",
+              number: 20,
+              price: 180000,
+          }, {
+        	  code: "100110001",
+        	  cycl: -0.25,
+        	  kind: "Mắt 1.56",
+        	  desciption: "Mắt cận loại thường",
+        	  number: 20,
+        	  price: 180000,
+          }, {
+              code: "100110001",
+              cycl: -0.25,
+              kind: "Mắt 1.56",
+              desciption: "Mắt cận loại thường",
+              number: 20,
+              price: 180000,
+          }, {
+        	  code: "100110001",
+        	  cycl: -0.25,
+        	  kind: "Mắt 1.56",
+        	  desciption: "Mắt cận loại thường",
+        	  number: 20,
+        	  price: 180000,
+          }, {
+        	  code: "100110001",
+        	  cycl: -0.25,
+        	  kind: "Mắt 1.56",
+        	  desciption: "Mắt cận loại thường",
+        	  number: 20,
+        	  price: 180000,
+          }, {
+        	  code: "100110001",
+        	  cycl: -0.25,
+        	  kind: "Mắt 1.56",
+        	  desciption: "Mắt cận loại thường",
+        	  number: 20,
+        	  price: 180000,
+          }, {
+        	  code: "100110001",
+        	  cycl: -0.25,
+        	  kind: "Mắt 1.56",
+        	  desciption: "Mắt cận loại thường",
+        	  number: 20,
+        	  price: 180000,
+          }, {
+        	  code: "100110001",
+        	  cycl: -0.25,
+        	  kind: "Mắt 1.56",
+        	  desciption: "Mắt cận loại thường",
+        	  number: 20,
+        	  price: 180000,
+          }, {
+        	  code: "100110001",
+        	  cycl: -0.25,
+        	  kind: "Mắt 1.56",
+        	  desciption: "Mắt cận loại thường",
+        	  number: 20,
+        	  price: 180000,
+          }, {
+        	  code: "100110001",
+        	  cycl: -0.25,
+        	  kind: "Mắt 1.56",
+        	  desciption: "Mắt cận loại thường",
+        	  number: 20,
+        	  price: 180000,
+          }, {
+        	  code: "100110001",
+        	  cycl: -0.25,
+        	  kind: "Mắt 1.56",
+        	  desciption: "Mắt cận loại thường",
+        	  number: 20,
+        	  price: 180000,
+          }, {
+        	  code: "100110001",
+        	  cycl: -0.25,
+        	  kind: "Mắt 1.56",
+        	  desciption: "Mắt cận loại thường",
+        	  number: 20,
+        	  price: 180000,
+          }, {
+              code: "100110001",
+              cycl: -0.25,
+              kind: "Mắt 1.56",
+              desciption: "Mắt cận loại thường",
+              number: 20,
+              price: 1800001,
+          } ],
+              $scope.searchKeywords = "",
+              $scope.filteredStores = [],
+              $scope.row = "", $scope.type = "",
+              $scope.select = function(page) {
+                  var end, start;
+                  if($scope.numPerPage == "tất cả") {
+                     start = 0, end = $scope.filteredStores.length;
+                  } else {
+                     start = (page - 1) * $scope.numPerPage,
+                     end = start + $scope.numPerPage;
+                  }
+                  $scope.currentPageStores = $scope.filteredStores.slice(start, end);
+                  for(var i = 1; i <= $scope.currentPageStores.length; i++) {
+                      $scope.currentPageStores[i-1].stt = start+i;
+                  }
+                  return $scope.currentPageStores;
+              },
+              $scope.onFilterChange = function() {
+                  return $scope.select(1), $scope.currentPage = 1, $scope.row = ""
+              },
+              $scope.onNumPerPageChange = function() {
+                  return $scope.select(1), $scope.currentPage = 1
+              },
+              $scope.onOrderChange = function() {
+                  return $scope.select(1), $scope.currentPage = 1
+              },
+              $scope.search = function() {
+                  return $scope.filteredStores = $filter("filter")($scope.stores, $scope.searchKeywords), $scope.onFilterChange()
+              },
+              $scope.order = function(rowName, type) {
+                  return ( $scope.row !== rowName || type !== $scope.type ) 
+                         ? ($scope.row = rowName, $scope.type = type, 
+                            $scope.filteredStores = $filter("orderBy")($scope.stores, rowName, $scope.type == "desc" ? true : false), $scope.onOrderChange()) : void 0
+              },
+              $scope.numPerPageOpt = [10, 15, 20, "tất cả"],
+              $scope.numPerPage = $scope.numPerPageOpt[1],
+              $scope.currentPage = 1,
+              $scope.currentPageStores = [],
+              (init = function() {
+                  return $scope.search(), $scope.select($scope.currentPage)
+              })();
+      }])
+  }.call(this),
+  function() {
         "use strict";
-        angular.module("app", ["app.tabs"]).config([function() {
+        angular.module("app", ["app.tabs", "app.tables", "ui.bootstrap"]).config([function() {
             
         }]).filter('NE', ['$sce', function($sce){
             return function(text) {
