824189bf06e9f3d4c83a869f40b2c64a8f4d2fb7
tinh1188 <tinhbv@d2tsolution.com>
2017-07-23 13:07:26 +0700

    #003

--- assets/js/common.js
+++ assets/js/common.js
@@ -44,3 +44,52 @@ function initShortCutByAreaId(areaId) {
         } , {propagate: false} );
     });
 }
+
+/**
+*
+* @param number
+* @return
+*/
+function readNumber(number) {
+console.log(number)
+   var billion = parseInt(Math.floor(number / 1000000000));
+console.log(billion)
+   number -= billion * 1000000000;
+
+   var million = parseInt(Math.floor(number / 1000000));
+   number -= million * 1000000;
+
+   var thousand = parseInt(Math.floor(number / 1000));
+   var unit = number - thousand * 1000;
+
+   var s = "";
+   if (billion != 0) {
+       s = s + readTriple(billion) + " tỷ, ";
+   }
+   if (million != 0) {
+       s = s + readTriple(million) + " triệu, ";
+   }
+   if (thousand != 0) {
+       s = s + readTriple(thousand) + " nghìn, ";
+   }
+   if (unit != 0) {
+       s = s + readTriple(unit);
+   }
+   s = s.trim().trim(",");
+   return s;
+}
+
+function readTriple(number) {
+   var HUNDRED_DIGITS = ["", "một trăm", "hai trăm", "ba trăm", "bốn trăm", "năm trăm", "sáu trăm", "bảy trăm", "tám trăm", "chín trăm"];
+   var TEN_DIGITS = [" ", " mười ", " hai mươi ", " ba mươi ", " bốn mươi ", " năm mươi ", " sáu mươi ", " bảy mươi ", " tám mươi ", " chín mươi "];
+   var UNIT_DIGITS = ["", "một", "hai", "ba", "bốn", "năm", "sáu", "bảy", "tám", "chín"];
+   var UNIT_DIGITS_2 = ["", "mốt", "hai", "ba", "tư", "lăm", "sáu", "bảy", "tám", "chín"];
+
+   var hundred = parseInt(Math.floor(number / 100));
+   number -= hundred * 100;
+   var ten = parseInt(Math.floor(number / 10));
+   var unit = number - ten * 10;
+
+   var s = HUNDRED_DIGITS[hundred] + TEN_DIGITS[ten] + (ten <= 1 ? UNIT_DIGITS[unit] : UNIT_DIGITS_2[unit]);
+   return s.trim();
+}
