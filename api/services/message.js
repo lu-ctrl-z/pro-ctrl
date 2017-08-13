3eb73bc38afe2de5d35a17dbe3ea6a04b9d6fefa
tinh1188 <tinhbv@d2tsolution.com>
2017-08-13 10:33:52 +0700

    #003

--- api/services/message.js
+++ api/services/message.js
@@ -1,8 +1,10 @@
 exports.of = function(model, validationError, i18n, ref) {
-    var validFields = Object.keys(validationError);
-    var i18nMessage = ref || {};
-    validFields.forEach(function(FieldsName) {
-        i18nMessage[model + '.' + FieldsName] = i18n(model + '.' + FieldsName + "." + validationError[FieldsName][0].rule);
-    });
-    return i18nMessage;
+    if(validationError) {
+        var validFields = Object.keys(validationError);
+        var i18nMessage = ref || {};
+        validFields.forEach(function(FieldsName) {
+            i18nMessage[model + '.' + FieldsName] = i18n(model + '.' + FieldsName + "." + validationError[FieldsName][0].rule);
+        });
+        return i18nMessage;
+    }
 };
